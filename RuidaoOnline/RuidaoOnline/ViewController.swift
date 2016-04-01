53670d9fd693037d1e336f1c2ebf0820cd1e6ded
MXR <MARUI@lab205bdeimac-18.local>
2016-04-01 09:11:23 +0800

    新建睿道APP项目

---
+++ RuidaoOnline/RuidaoOnline/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RuidaoOnline
+//
+//  Created by MXR on 16/4/1.
+//  Copyright © 2016年 MXR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
